c12374a5c504299d55e6faa09891f5d525a92ae0
Ben Gamari <ben@smart-cactus.org>
2015-08-14 17:09:23 +0200

    Add command line parsing

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 import Data.Monoid
 import Data.Generics
 import Data.Foldable
@@ -6,21 +8,49 @@ import DynFlags
 import qualified GHC
 import qualified GHC.Paths
 import Digraph (flattenSCCs) -- this should be expected from GHC
+import Options.Applicative hiding ((<>))
 
-import Outputable
+import Outputable hiding ((<>))
+
+newtype Matcher = Matcher
+    { runMatcher :: forall r. Monoid r => (GHC.LHsBind GHC.Id -> r) -> GHC.LHsBinds GHC.Id -> GHC.Ghc r }
+
+data Opts = Opts { sourceFiles :: [FilePath]
+                 , matcher     :: Matcher
+                 }
+
+pureMatcher :: (a -> GHC.Ghc b)
+            -> (forall r. Monoid r => b -> (GHC.LHsBind GHC.Id -> r) -> GHC.LHsBinds GHC.Id -> r)
+            -> a -> Matcher
+pureMatcher prepare match x =
+    Matcher $ \f binds -> prepare x >>= \y -> pure $ match y f binds
+
+opts = Opts
+       <$> many (strArgument $ metavar "MODULE.hs" <> help "Haskell source modules to search within")
+       <*> (typeContains <|> ofType)
+  where
+    typeContains = pureMatcher lookupType foldBindsContainingType
+                   <$> strOption (  long "containing"
+                                 <> help "Find bindings whose type mentions the given type"
+                                 <> metavar "TYPE")
+    ofType = pureMatcher lookupType foldBindsOfType
+             <$> strOption (   long "of-type"
+                            <> help "Find bindings of the given type"
+                            <> metavar "TYPE")
 
 main = do
-    GHC.runGhc (Just $ GHC.Paths.libdir) action
+    args <- execParser $ info (helper <*> opts) mempty
+    GHC.runGhc (Just GHC.Paths.libdir) (runMatch args)
 
-action :: GHC.Ghc ()
-action = GHC.defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
+runMatch :: Opts -> GHC.Ghc ()
+runMatch args = GHC.defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
     -- Note that this initial {get,set}SessionDynFlags is not idempotent
     dflags <- GHC.getSessionDynFlags
     GHC.setSessionDynFlags dflags { hscTarget = HscNothing }
     let printSDoc :: SDoc -> GHC.Ghc ()
         printSDoc = liftIO . putStrLn . showSDoc dflags
 
-    GHC.setTargets [GHC.Target (GHC.TargetFile "Test.hs" Nothing) True Nothing]
+    GHC.setTargets (map (\f -> GHC.Target (GHC.TargetFile f Nothing) True Nothing) $ sourceFiles args)
     summaries <- GHC.depanal [] True
     let graph = flattenSCCs $ GHC.topSortModuleGraph True summaries Nothing
     let processModule :: GHC.ModSummary -> GHC.Ghc GHC.TypecheckedModule
@@ -32,20 +62,17 @@ action = GHC.defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
     let modNames = map (GHC.moduleName . GHC.ms_mod) graph
     GHC.setContext $ map GHC.IIModule modNames
 
-    (ty, _) <- GHC.typeKind False "TestType"
-    (ty', _) <- GHC.typeKind False "ThisIsAType"
-    printSDoc $ vcat $ concatMap (map pprLocated
-                                  . foldBindsOfType ty (\a->[a])
-                                  . GHC.tm_typechecked_source)
-                     $ toList typechecked
-
-    printSDoc $ vcat $ concatMap (map pprLocated
-                                  . foldBindsContainingType ty (\a->[a])
-                                  . GHC.tm_typechecked_source)
-                     $ toList typechecked
+    matches <- concat <$> mapM (runMatcher (matcher args) (\a->[a])
+                                . GHC.tm_typechecked_source)
+                               (toList typechecked)
+    printSDoc $ vcat $ map pprLocated matches
 
 pprLocated :: (Outputable l, Outputable e) => GHC.GenLocated l e -> SDoc
 pprLocated (GHC.L l e) = braces (ppr l) $$ nest 4 (ppr e)
+
+-- | Thows error if not in scope
+lookupType :: String -> GHC.Ghc GHC.Type
+lookupType tyName = fst <$> GHC.typeKind False tyName
 
 foldBindsOfType :: (Monoid r)
                 => GHC.Type -> (GHC.LHsBind GHC.Id -> r)
